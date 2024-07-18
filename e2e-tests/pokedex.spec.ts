a725a8eeb753045c8c7c25088dc89f3dab4f2b3d
OviniP <149691951+OviniP@users.noreply.github.com>
2024-07-18 19:30:59 +0300

    new rest added

--- e2e-tests/pokedex.spec.ts
+++ e2e-tests/pokedex.spec.ts
@@ -2,8 +2,16 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-     await page.goto('http://localhost:8080')
+    await page.goto('http://localhost:8080/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('Can navigate to detail page', async({ page }) => {
+    await page.goto('http://localhost:8080/')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    const locator =  page.getByText('ivysaur')
+    await locator.click()
+    await expect(page.getByText('ivysaur')).toBeVisible()
   })
 })
